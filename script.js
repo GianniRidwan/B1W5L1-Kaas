48f6be6ad9f81858609a37f34e89ba7d4795f0f8
Gianni Ridwan <99036552@mydavinci.nl>
2020-09-25 09:23:55 +0200

    Added .toLowerCase()

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const nee = false;
 
 var cheese = prompt("Is de kaas geel?");
 
-if (cheese == "ja") {
+if (cheese.toLowerCase() == "ja") {
 	var holes = prompt("Zitten er gaten in?");
 	
-	if (holes == "ja") {
+	if (holes.toLowerCase() == "ja") {
 		var expensive = prompt("Is de kaas belachelijk duur?");
 	
-		if (expensive == "ja")
+		if (expensive.toLowerCase() == "ja")
 			alert("Emmenthaler");
 		else
 			alert("Leerdammer");
@@ -21,7 +21,7 @@ if (cheese == "ja") {
 	else 
 		var hard = prompt("Is de kaas hard als steen?");
 		
-		if (hard == "ja")
+		if (hard.toLowerCase() == "ja")
 			alert("Parmigiano Reggiano");
 		else
 			alert("Goudse kaas");
@@ -30,10 +30,10 @@ if (cheese == "ja") {
 else {
 	var fungus = prompt("Heeft de kaas blauwe schimmels?");
 
-	if (fungus == "ja") {
+	if (fungus.toLowerCase() == "ja") {
 		var crust = prompt("Heeft de kaas een korst?");
 
-		if (crust == "ja")
+		if (crust.toLowerCase() == "ja")
 			alert("Bleu de Rochbaron");
 		else
 			alert("Fourme d'Ambert");
@@ -41,7 +41,7 @@ else {
 	else {
 		var secondCrust = prompt("Heeft de kaas een korst?");
 
-		if (secondCrust == "ja")
+		if (secondCrust.toLowerCase() == "ja")
 			alert("Camembert");
 		else
 			alert("Mozzarella");
